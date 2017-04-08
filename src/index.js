c3ab26aed209bb5c1fa0cbfcdc0884c5261092d2
Stijn de Witt <StijnDeWitt@hotmail.com>
2017-04-08 17:52:41 +0200

    Basic working version with composing of mics included

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
 
 
+// should combine `mix` and `mixin`
+// * no arguments: creates a new base class that has a `with` method to mix in other mics later
+// * `superclass` argument: 
+//    - optional, if supplied it must be a ES6 class
+//    - if the superclass is a `type` (has an `interface` property), return the superclass (extended by the given mixins if any)
+//    - if superclass is not a `type`, create a derived class from it, add an `interface` property and return the derived class (extended by the given mixins if any)
+// * optional es6 class as first argument
+//   --> if supplied, return an ES6 class, otherwise a mixin
+// * optional 0..n mixins
+//   --> will be mixed into resulting class/mixin
+// * optional class factory as last argument
+//   --> will be last in prototype chain
+//
+// should accept either an es6 class + optional mics
+// or optional mics and an optional class factory
+// when invoked with a single mics, return it unchanges
+// 
+// var MixinA = mics(superclass => class MixinA extends superclass {
+//   a(){}
+// })
+// 
+// var MixinB = mics(superclass => class MixinB extends superclass {
+//   b(){}
+// })
+// 
+// class Mixed extends mics(MixinA, MixinB, )
+
+export function mics(...args){
+	var superclass = args.length && isFunction(args[0]) && isClass(args[0]) && args.shift()
+	var factory = args.length && isFunction(args[args.length-1]) && !isMics(args[args.length-1]) && isFactory(args[args.length-1]) && args.pop()
+	// if neither superclass nor factory are provided, create a new superclass
+	if (!superclass && !factory) superclass = class{}
+	if (superclass) {
+		if (!isMics(superclass)) {
+			superclass = class extends superclass {}
+			Object.defineProperties(superclass, {
+				with: {get:() => (...mixins) => mixins.reduce((c,m) => m(c), superclass)},
+				interface: {get:(x => () => x ? x : x = getInterface(superclass.prototype))()}
+			})
+		}
+		return args.length ? superclass.with(args) : superclass
+	}
+
+	if (args.length) factory = (org => superclass => org(args.reduce((c,m) => m(c), superclass)))(factory)
+	function mixin(superclass, ...args) {
+		if (this instanceof mixin) return new mixin.class(superclass, ...args)
+		var result = is(superclass).a(mixin) ? superclass : factory(superclass)
+		if (mixin.classes.indexOf(result) == -1) mixin.classes.push(result)
+		return result
+	}
+	Object.defineProperties(mixin, {classes: {value:[], writable:false}})
+	// has to be 2 steps because mixin adds the created class to mixin.classes
+	Object.defineProperties(mixin, {
+		class: {value:mixin(class {}), writable:false},
+		with: {get:() => (...mixins) => mics(mixin.class, mixins)},
+		interface: {get:(x => () => x ? x : x = getInterface(mixin.class.prototype))()}
+	})
+	return mixin
+}
+
 export function mix(superclass, ...mixins) {
 	if (! superclass) superclass = class{}
 	if (! superclass.with) {
@@ -11,8 +71,6 @@ export function mix(superclass, ...mixins) {
 	}
 	return mixins.length ? superclass.with(mixins) : superclass
 }
-
-export default mix
 
 export function mixin(fn) {
 	function mixin(superclass, ...args) {
@@ -63,6 +121,7 @@ function getInterface(proto) {
 	return Object.getOwnPropertyNames(proto).reduce((o,k) => {o[k] = proto[k]; return o}, {})
 }
 
-function isNativeClass (value) {
-	return typeof value === 'function' && value.toString().indexOf('class') === 0
-}
+var isFunction = x => typeof x == 'function'
+var isClass = x => x.toString().indexOf('class') === 0
+var isMics = x => !!(x.interface && x.with)
+var isFactory = x => x.length === 1
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,117 +2,44 @@ import { expect } from 'chai'
 import ulog from 'ulog'
 const log = ulog('mics:spec')
 
-import { mixin, mix, is } from './'
+import { mics, is } from './'
 
 describe('mics', function(){
-	it('Multiple Inheritance Class System', function(){})
-	it('intuitive mixins for ES6 classes', function(){})
-
-	describe('mixin', function(){
-		it('is a function', function(){
-			expect(mixin).to.be.a('function')		
-		})
-		it('creates a mixin from an ES6 class factory', function(){
-			var M = mixin(superclass => class M extends superclass {})
-			expect(M).to.be.a('function')
-		})
-		it('creates a mixin that can be invoked with new', function(){
-			var M = mixin(superclass => class M extends superclass {})
-			var m = new M()
-			expect(m).to.be.an('object')
-		})
-		it('composes multiple mixins', function(){
-			var Looker = mixin(superclass => class Looker extends superclass {
-				look(){log.info('Looking good!')}
-			})
-			var Walker = mixin(superclass => class Walker extends superclass {
-				walk(){log.info('Step, step, step...')}
-			})
-			var Talker = mixin(superclass => class Talker extends superclass {
-				talk(){log.info('Blah, blah, blah...')}
-			})
-			var Duck = mixin(Looker, Walker, Talker, superclass => class Duck extends superclass {
-				talk(){log.info('Quack, quack, quack!')}
-			})
-
-			var duck = new Duck()
-			log.info('duck=', duck)
-			duck.talk()
-		})
+	it('is a function', function(){
+		expect(mics).to.be.a('function')		
 	})
 
-	describe('mix', function(){
-		it('is a function', function(){
-			expect(mix).to.be.a('function')		
-		})
+	it('creates a mixin from an ES6 class factory', function(){
+		var M = mics(superclass => class M extends superclass {})
+		expect(M).to.be.a('function')
 	})
 
-	describe('is', function(){
-		it('is a function', function(){
-			expect(is).to.be.a('function')		
-		})
+	it('creates a mixin that can be invoked with new', function(){
+		var M = mics(superclass => class M extends superclass {})
+		var m = new M()
+		expect(m).to.be.an('object')
 	})
 
-	it('supports mixins', function(){
-		var Driver = mixin(superclass => class Driver extends superclass {  
-			constructor() {
-				super()
-				log.info('A driver is born!')
-			}
-			drive() {
-				log.info('Vroom Vroom!!')
-			}
+	it('composes multiple mixins', function(){
+		var Looker = mics(superclass => class Looker extends superclass {
+			look(){log.info('Looking good!')}
 		})
 
-		var Swimmer = mixin(superclass => class extends superclass {  
-			constructor() {
-				super()
-				log.info('A swimmer is born!')
-			}
-			swim() {
-				log.info('Splash splash!')
-			}
-			dive() {
-				log.info('Blub blub!')
-			}
+		var Walker = mics(superclass => class Walker extends superclass {
+			walk(){log.info('Step, step, step...')}
 		})
 
-		class Sporter extends mix().with(Driver, Swimmer) {
-			constructor() {
-				super()
-				log.info('A sporter is born!')
-			}
-			sport() {
-				this.swim()
-				this.drive()
-			}
-		}
-
-		class F1Driver {
-			drive() {
-				
-			}
-		}
-
-		var sporter = new Sporter()
-		log.info('sporter: ', sporter)
-		sporter.sport()
-
-		var shumacher = new F1Driver()
-
-		log.info('is sporter a Swimmer? ', is(sporter).a(Swimmer))
-		log.info('is sporter a Driver? ', is(sporter).a(Driver))
-		log.info('is sporter a Sporter? ', is(sporter).a(Sporter))
-		log.info('is object as Swimmer? ', is({swim(){}, dive(){}}).as(Swimmer))
-		log.info('is shumacher as Driver? ', is(shumacher).as(Driver))
-		log.info('is F1Driver a Driver? ', is(F1Driver).a(Driver))
-		log.info('is F1Driver as Driver? ', is(F1Driver).as(Driver))
-		log.info('is F1Driver as Swimmer? ', is(F1Driver).as(Swimmer))
-
-		window.genericDriver = new Driver();
-		log.info('genericDriver: ', genericDriver)
-		log.info('is genericDriver a Driver? ', is(genericDriver).a(Driver))
+		var Talker = mics(superclass => class Talker extends superclass {
+			talk(){log.info('Blah, blah, blah...')}
+		})
 		
-		log.info()
+		var Duck = mics(Looker, Walker, Talker, superclass => class Duck extends superclass {
+			talk(){log.info('Quack, quack, quack!')}
+		})
+
+		window.duck = new Duck()
+		log.info('duck=', duck)
+		duck.talk()
 	})
 })
+
