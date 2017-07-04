068ef364c6ed01418732d60840f66ca956b696a6
minecrawler <me@marco-alka.de>
2017-07-04 16:33:47 +0200

    change tests according to lint, adjust linting rules, adjust src according to rule changes

--- src/index.js
+++ src/index.js
@@ -1,97 +1,97 @@
 export { mix, is, like }
 
 function mix(...args) {
-	// todo: refactor to make const
-	let superclass = !is(args[0], 'factory') && args.shift() || baseclass
-	let factory = (is(args[args.length-1], 'factory') && args.pop()) || derive
+    // todo: refactor to make const
+    let superclass = !is(args[0], 'factory') && args.shift() || baseclass
+    let factory = (is(args[args.length-1], 'factory') && args.pop()) || derive
 
-	superclass = is(superclass, 'mixin') ? superclass.class : derive(superclass)
-	if (args.length) factory = (org => superclass => org(args.reduce((s,m) => m.mixin(s), superclass)))(factory)
+    superclass = is(superclass, 'mixin') ? superclass.class : derive(superclass)
+    if (args.length) factory = (org => superclass => org(args.reduce((s,m) => m.mixin(s), superclass)))(factory)
 
-	function mixin(superclass) {
-		const result = is(superclass, mixin) ? superclass : factory(superclass)
+    function mixin(superclass) {
+        const result = is(superclass, mixin) ? superclass : factory(superclass)
 
-		if (mixin.classes.indexOf(result) === -1) mixin.classes.push(result)
-		return result
-	}
+        if (mixin.classes.indexOf(result) === -1) mixin.classes.push(result)
+        return result
+    }
 
-	Object.defineProperties(mixin, {
-		classes: {value:[], writable:false},
-		mixins: {value:args, writable:false},
-	})
+    Object.defineProperties(mixin, {
+        classes: { value:[], writable:false },
+        mixins: { value:args, writable:false },
+    })
 
-	const Class = mixin(superclass)
-	const constructor = Class.hasOwnProperty('constructor')
-		? Class.constructor.bind(Class)
-		: (...args) => new Class(...args)
+    const Class = mixin(superclass)
+    const constructor = Class.hasOwnProperty('constructor')
+        ? Class.constructor.bind(Class)
+        : (...args) => new Class(...args)
 
-	Object.getOwnPropertyNames(Class).forEach(k => Object.defineProperty(constructor, k, {value: Class[k]}))
+    Object.getOwnPropertyNames(Class).forEach(k => Object.defineProperty(constructor, k, { value: Class[k] }))
 
-	return Object.defineProperties(constructor, {
-		mixin: {value:mixin, writable:false},
-		class: {value: Class, writable:false},
-		interface: {get:(x => () => x ? x : x = getInterface(Class.prototype))()},
-	})
+    return Object.defineProperties(constructor, {
+        mixin: { value:mixin, writable:false },
+        class: { value: Class, writable:false },
+        interface: { get:(x => () => x ? x : x = getInterface(Class.prototype))() },
+    })
 }
 
 function is(x, type) {
-	if (typeof type == 'string') {
-		return type == 'class'
-			? (
-				is(x, 'function') && (s => /^class\s/.test(s)
-				|| /^.*classCallCheck\(/.test(s.replace(/^[^{]*{\s*/,'').replace(/\s*}[^}]*$/,'')))(x.toString())
-			)
-			: type == 'mixin'
-				? is(x, 'function') && !!x.mixin
-				: type == 'factory'
-					? is(x, 'function') && !is(x, 'mixin') && !is(x, 'class') && x.length == 1
-					: typeof x == type;
-	}
+    if (typeof type == 'string') {
+        return type == 'class'
+            ? (
+                is(x, 'function') && (s => /^class\s/.test(s)
+    || /^.*classCallCheck\(/.test(s.replace(/^[^{]*{\s*/,'').replace(/\s*}[^}]*$/,'')))(x.toString())
+            )
+            : type == 'mixin'
+                ? is(x, 'function') && !!x.mixin
+                : type == 'factory'
+                    ? is(x, 'function') && !is(x, 'mixin') && !is(x, 'class') && x.length == 1
+                    : typeof x == type;
+    }
 
-	if (typeof x == 'object') {
-		if (x instanceof type) return true
-		if (type.class && x instanceof type.class) return true
-		if (type.mixin && type.mixin.classes) return type.mixin.classes.reduce((f,c) => f || is(x,c), false)
-	}
-	else if (typeof x == 'function') {
-		if (x.mixin && x.mixin.mixins.indexOf(type) !== -1) return true
+    if (typeof x == 'object') {
+        if (x instanceof type) return true
+        if (type.class && x instanceof type.class) return true
+        if (type.mixin && type.mixin.classes) return type.mixin.classes.reduce((f,c) => f || is(x,c), false)
+    }
+    else if (typeof x == 'function') {
+        if (x.mixin && x.mixin.mixins.indexOf(type) !== -1) return true
 
-		let c = x
+        let c = x
 
-		while (c !== Object) {
-			if (c === type || c === type.class) return true
-			if (type.mixin && type.mixin.classes && type.mixin.classes.indexOf(c) !== -1) return true
-			c = Object.getPrototypeOf(c.prototype).constructor
-		}
-	}
+        while (c !== Object) {
+            if (c === type || c === type.class) return true
+            if (type.mixin && type.mixin.classes && type.mixin.classes.indexOf(c) !== -1) return true
+            c = Object.getPrototypeOf(c.prototype).constructor
+        }
+    }
 
-	return false
+    return false
 }
 
 function like(x, type) {
-	if (is(x, type)) return true
+    if (is(x, type)) return true
 
-	const itf = type.interface || (is(type, 'function') && getInterface(type.prototype))
-	const subject = is(x, 'function') ? x.interface || getInterface(x.prototype) : x
+    const itf = type.interface || (is(type, 'function') && getInterface(type.prototype))
+    const subject = is(x, 'function') ? x.interface || getInterface(x.prototype) : x
 
-	return itf && Object.keys(itf).reduce((f, k) => 
-		f && (is(itf[k], 'function') ? is(subject[k], 'function') : k in subject), true
-	)
+    return itf && Object.keys(itf).reduce((f, k) => 
+        f && (is(itf[k], 'function') ? is(subject[k], 'function') : k in subject), true
+    )
 }
 
 function getInterface(proto) {
-	return getPropertyNames(proto).reduce((o,k) => { o[k] = proto[k]; return o }, {})
+    return getPropertyNames(proto).reduce((o,k) => { o[k] = proto[k]; return o }, {})
 }
 
 function getPropertyNames(proto) {
-	const results = []
+    const results = []
 
-	while (proto !== Object.prototype) {
-		Object.getOwnPropertyNames(proto).reduce((arr,k) => arr.indexOf(k) === -1 ? arr.push(k) && arr : arr, results)
-		proto = Object.getPrototypeOf(proto).constructor.prototype
-	}
+    while (proto !== Object.prototype) {
+        Object.getOwnPropertyNames(proto).reduce((arr,k) => arr.indexOf(k) === -1 ? arr.push(k) && arr : arr, results)
+        proto = Object.getPrototypeOf(proto).constructor.prototype
+    }
 
-	return results
+    return results
 }
 
 const baseclass = class Object{}
